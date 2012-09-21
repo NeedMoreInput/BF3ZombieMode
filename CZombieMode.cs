a61b9efb16867980d8f737369ccd5c49e92c9155
PC9 <PapaCharlie9@gmail.com>
2012-09-21 12:20:24 -0700

    update copyright

--- CZombieMode.cs
+++ CZombieMode.cs
@@ -1,19 +1,5 @@
-/*  Copyright 2010 Geoffrey 'Phogue' Green
+/*  CZombieMode - Copyright 2012 m4xx
 
-    This file is part of BFBC2 PRoCon.
-
-    BFBC2 PRoCon is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    BFBC2 PRoCon is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with BFBC2 PRoCon.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
