ee4928c8abfeeae94a748fd31ea66eb3c102e547
PapaCharlie9 <PapaCharlie9@gmail.com>
2012-10-19 11:49:18 -0700

    Bumped version, copyright

--- CZombieMode.cs
+++ CZombieMode.cs
@@ -1,6 +1,16 @@
-/*  CZombieMode - Copyright 2012 m4xx
+/*  CZombieMode - Copyright 2012 by PapaCharlie9, m4xx
 
- */
+Permission to use, copy, modify, and/or distribute this software for any purpose 
+with or without fee is hereby granted without restriction.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES 
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF 
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR 
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES 
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN 
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT 
+OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
 
 using System;
 using System.ComponentModel;
@@ -1440,7 +1450,7 @@ namespace PRoConEvents
 
 		public string GetPluginVersion()
 		{
-			return "0.1.5.0";
+			return "0.1.6.0";
 		}
 
 		public string GetPluginAuthor()
